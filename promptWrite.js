a0f68ee342bc65225f2736cdc240a1383a36b6cf
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-03-23 15:55:34 -0700

    prompt post functionality

--- promptWrite.js
+++ promptWrite.js
@@ -4,7 +4,7 @@
 
 var app = angular.module('promptWriteApp', []);
 
-app.service('promptWriteService', function() {
+app.service('promptWriteService', function($http) {
     var selfServ = this;
     this.dataServ = null;
 
@@ -15,7 +15,7 @@ app.service('promptWriteService', function() {
             url: 'edit_prompts.php',
             method: 'post',
             cache: false,
-            headers : {'Content-Type': 'application/x-www-form-urlencoded'},
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
             data: $.param({
                 mode: 'add',
                 title: 'Dummy Title 1',
@@ -25,35 +25,15 @@ app.service('promptWriteService', function() {
             })
         })
             .then(
-                function(response){
+                function (response) {
                     console.log('Success', response);
                 },
-                function(error){
-                    console.log('Failure',error);
+                function (error) {
+                    console.log('Failure', error);
                 }
             );
-
-        /*$.ajax({
-            url: 'edit_prompts.php',
-            method: 'post',
-            cache: false,
-            dataType: 'json',
-            data: {
-                mode: 'add',
-                title: 'Dummy Title 1',
-                description: 'Dummy Prompt 1',
-                genre: 'Dummy Genre 1',
-                setting: 'Dummy Setting 1'
-            },
-            success: function (response) {
-                console.log('Success', response);
-                selfServ.dataServ = response.data;
-            },
-            error: function (response) {
-                console.log('Failure', response);
-            }
-        });*/
-};
+    };
+});
 
 app.controller('promptWriteController', function(promptWriteService){
     this.post = promptWriteService.postPrompt;
