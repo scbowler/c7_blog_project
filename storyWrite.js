fc6f022f92d37b3ec667f131c2ac89477ebd59a0
Nicholas Bidler <nomandoestheimpossible@gmail.com>
2016-03-23 10:27:46 -0700

    basic server response check added

--- storyWrite.js
+++ storyWrite.js
@@ -4,10 +4,34 @@
 
 var app = angular.module('storyWriteApp', []);
 
-app.service('storyWriteService', function(){
+app.service('storyWriteService', function($http){
 
+    var selfServ = this;
+    this.dataServ = null;
+
+    this.getPrompt = function(promptID){
+        console.log('Attempting to contact server');
+        $http({
+            url: 'get_prompts.php',
+            method: 'post',
+            cache: false
+        })
+            .then(
+                function(response){
+                    console.log('Success', response);
+                    selfServ.dataServ = response;
+                },
+                function(response){
+                    console.log('Failure',response);
+                    selfServ.dataServ = response;
+                }
+            );
+    }
 });
 
 app.controller('storyWriteController', function(storyWriteService){
+    var selfCont = this;
 
+    storyWriteService.getPrompt(1);
 });
+
