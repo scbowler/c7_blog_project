77ef09de60586c67d89755f1701db9bb3c3032a9
kkennethlee <kyung.kenneth.lee@gmail.com>
2016-03-23 10:04:04 -0700

    added regex condition to new user registration

--- newuserHandler.php
+++ newuserHandler.php
@@ -17,16 +17,29 @@
         ['field'=>$email, 'regex'=>'/^\S+@\S+$/', 'error_message'=>'The email must be in a valid format'],
         ['field'=>$password, 'regex'=>'/[a-zA-Z0-9]{5,32}/', 'error_message'=>'name must be 3 letters or more']
     ];
-    if(preg_match($regex[0]['regex'], $username) && preg_match($regex[1]['regex'], $password)) {
+    if(preg_match($regex[0]['regex'], $username) && preg_match($regex[1]['regex'], $email) && preg_match($regex[2]['regex'], $password)) {
         $query = "INSERT INTO `users` (`username`, `email`, `password`) VALUES ('$username', '$email', '$password')";
         $result = mysqli_query($connect, $query);
         $output = [
             'success' => true,
             'results' => $result
         ];
-        $output_string = json_encode($output);
-        //print_r($_POST);
-        print($output_string);
-        //echo $query;
+    } else {
+        $output = [
+            'success' => false
+        ];
+        if(!preg_match($regex[0]['regex'], $username)){
+            $output['usernameMessage'] = 'Username must be 3 to 16 characters long.';
+        }
+        if(!preg_match($regex[1]['regex'], $email)){
+            $output['emailMessage'] = 'Email must be in a valid format.';
+        }
+        if(!preg_match($regex[2]['regex'], $password)){
+            $output['passwordMessage'] = 'Password must be alphanumeral characters between 5 to 32 characters long';
+        }
     }
+    $output_string = json_encode($output);
+    //print_r($_POST);
+    print($output_string);
+    //echo $query;
 ?>
