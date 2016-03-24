6bd345a33934f0a20ef850156935c7cee1518b40
Tim Horist Jr <Tim Horist Jr>
2016-03-23 18:41:04 -0700

    Redo of kenneth branch

---
+++ password_encryptor.config.php
@@ -0,0 +1,10 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Kenneth
+     * Date: 3/23/2016
+     * Time: 6:15 PM
+     */
+    /*function encryptPassword('input1', 'input2', 'input3' ...){
+    }*/
+?>
